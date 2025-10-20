c4d61b0ab5544741da6c3524420cd33187703a32
GRAY-XY <zilinwangleo@gmail.com>
2025-10-20 20:21:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from src.prompt_builder import generate_prompts
 
+if __name__ == "__main__":
+    path = input("请输入图片路径：").strip('"')
+    caption, pos, neg = generate_prompts(path)
+    
+    print("\n 图像描述：", caption)
+    print("\n 正向提示词：\n", pos)
+    print("\n 负向提示词：\n", neg)
